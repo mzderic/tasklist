d1453738cd2193fda565b0ea2767ce69ccfd2f45
Maja <majslcr@yahoo.com>
2017-10-20 08:05:55 +0200

    napravljen master blade view app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+<html lang="hr">
+	<head>
+		<title>TASK app -Laravel vježba</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<!-- Navbar sadržaj -->
+			</nav>
+		</div>
+		@yield('content')
+	</body>
+</html>
