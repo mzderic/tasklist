9dd3236fc2314a248b6a7654c03386fe74218aab
Maja <majslcr@yahoo.com>
2017-10-24 00:37:54 +0200

    dodan common.errors view

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,12 @@
+@if (count($errors) > 0)
+	<div class="alert alert-danger">
+	<strong>Uuuuupss, nešto je pošlo krivo!>/strong>
+	<br><br>
+	<ul>
+		@foreach ($errors->all() as $error)
+				<li>{{ $error}}</li>
+		@endforeach
+	</ul>
+	</div>
+
+@endif
